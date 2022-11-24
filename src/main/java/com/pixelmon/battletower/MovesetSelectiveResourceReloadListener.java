2cf0c04d17e22bcbad1725ab8d9fe98f9f358d78
Paul Steele <paul@paul-steele.com>
2022-11-24 17:18:05 -0500

    refactor moveset loader

--- src/main/java/com/pixelmon/battletower/MovesetSelectiveResourceReloadListener.java
+++ src/main/java/com/pixelmon/battletower/MovesetSelectiveResourceReloadListener.java
@@ -10,6 +10,7 @@ import com.pixelmonmod.pixelmon.api.pokemon.ability.Ability;
 import com.pixelmonmod.pixelmon.api.pokemon.ability.AbilityRegistry;
 import com.pixelmonmod.pixelmon.api.pokemon.species.Species;
 import com.pixelmonmod.pixelmon.api.pokemon.species.moves.Moves;
+import com.pixelmonmod.pixelmon.api.pokemon.stats.EVStore;
 import com.pixelmonmod.pixelmon.api.pokemon.stats.IVStore;
 import com.pixelmonmod.pixelmon.api.pokemon.stats.Moveset;
 import com.pixelmonmod.pixelmon.api.registries.PixelmonSpecies;
@@ -18,6 +19,7 @@ import com.pixelmonmod.pixelmon.battles.attacks.ImmutableAttack;
 import com.pixelmonmod.pixelmon.battles.attacks.specialAttacks.basic.HiddenPower;
 import net.minecraft.resources.IResourceManager;
 import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.Tuple;
 import net.minecraftforge.resource.IResourceType;
 import net.minecraftforge.resource.ISelectiveResourceReloadListener;
 
@@ -61,20 +63,6 @@ public class MovesetSelectiveResourceReloadListener implements ISelectiveResourc
     }
 
     private void CreateMoveSet(List<String> input){
-        String name = "";
-        String form = "";
-        String item = "";
-        String abilityName = "";
-        int hpEV = 0;
-        int atkEV = 0;
-        int defEv = 0;
-        int spAtkEv = 0;
-        int spDefEv = 0;
-        int speEv = 0;
-        IVStore ivStore = new IVStore(31, 31, 31, 31, 31, 31);
-        String natureName = "";
-        List<String> moves = new ArrayList<>();
-
         String[] lines = input.toArray(new String[0]);
 
         long numMoves = Arrays.stream(lines).filter(l -> l.startsWith("-")).count();
@@ -90,149 +78,221 @@ public class MovesetSelectiveResourceReloadListener implements ISelectiveResourc
             lines = newLines;
         }
 
-        String[] line1Split = lines[0].split("@");
-        name = line1Split[0].trim();
-        if (line1Split.length == 2){
-            item = line1Split[1].trim();
-        }
+        SmogonLineOne nameFormItem = new SmogonLineOne(lines[0]);
+        SmogonLineTwo ability = new SmogonLineTwo(lines[1]);
+        SmogonLineThree evs = new SmogonLineThree(lines[2]);
+        SmogonLineFour nature = new SmogonLineFour(lines[3]);
+        SmogonFinalLines movesAndIvs = new SmogonFinalLines(Arrays.stream(lines).skip(4).toArray(String[]::new));
 
-        String[] nameSplit = name.split("-");
-        if (nameSplit.length >= 2){
-
-            name = nameSplit[0].trim();
-            form = nameSplit[1].trim()
-                    .replace("Alola", "alolan")
-                    .replace("Galar", "galarian");
-
-            if (nameSplit.length > 2){
-                form = nameSplit[1] + nameSplit[2];
-            }
-
-            if (form.equals("F")){
-                form = "female";
-            }
-
-            if (form.equals("o")){
-                name = name + "-o";
-                form = "";
-            }
-
-            if (form.equals("Z")){
-                name = name + "-Z";
-                form = "";
-            }
-
-            if (form.equals("Oh")){
-                name = name + "-Oh";
-                form = "";
-            }
-
-            if (form.equals("Dawn Wings")){
-                form = "dawn";
-            }
-
-            if (form.equals("Dusk Mane")){
-                form = "dusk";
-            }
-
-            if (form.equals("Small") || form.equals("Super") || form.equals("Large")){
-                form = "";
-            }
-        }
-
-        abilityName = lines[1].replace("Ability:", "").trim().replace(" ", "");
-        String[] evs = lines[2].replace("EVs:", "").split("/");
-        for (String ev : evs) {
-            String[] split = ev.trim().split(" ");
-            if (split.length != 2){
-                break;
-            }
-            switch (split[1].trim()){
-                case "HP": {
-                    hpEV = Integer.parseInt(split[0]);
-                    break;
-                }
-                case "Atk": {
-                    atkEV = Integer.parseInt(split[0]);
-                    break;
-                }
-                case "Def": {
-                    defEv = Integer.parseInt(split[0]);
-                    break;
-                }
-                case "SpA": {
-                    spAtkEv = Integer.parseInt(split[0]);
-                    break;
-                }
-                case "SpD": {
-                    spDefEv = Integer.parseInt(split[0]);
-                    break;
-                }
-                case "Spe": {
-                    speEv = Integer.parseInt(split[0]);
-                    break;
-                }
-            }
-        }
-        natureName = lines[3].replace("Nature", "").trim();
-        for (int i = 0; i < numMoves; i++){
-            String moveString = lines[4 + i].substring(2).trim();
-
-            String[] hiddenPowerSplit = moveString.split("Hidden Power");
-            if (hiddenPowerSplit.length == 2){
-                moveString = "Hidden Power";
-                Element ele = Element.parseType(hiddenPowerSplit[1].trim());
-                ivStore = HiddenPower.getOptimalIVs(ele);
-            }
-
-            moves.add(moveString);
-        }
-
-        Optional<Species> species = PixelmonSpecies.fromName(name).getValue();
-        if (!species.isPresent()){
-            Logger.getGlobal().info("Could not get species from name " + name);
+        if (!nameFormItem.Species.isPresent()){
             return;
         }
-        Optional<Ability> ability =  AbilityRegistry.getAbility(abilityName);
-        if (!ability.isPresent()){
-            //some abilities have mismatched spellings
-            abilityName = abilityName
-                    .replace("ShellArmor", "ShellArmour")
-                    .replace("BattleArmor", "BattleArmour");
-
-            ability =  AbilityRegistry.getAbility(abilityName);
-            if (!ability.isPresent()){
-                Logger.getGlobal().info("Could not get ability from name " + abilityName);
-                return;
-            }
+        if (!ability.Ability.isPresent()){
+            return;
         }
-        Nature nature = Nature.natureFromString(natureName);
-        if (nature == null){
-            Logger.getGlobal().info("Could not get nature from name " + natureName);
+        if (!nature.NatureValue.isPresent()){
+            return;
+        }
+        if (!movesAndIvs.Moveset.isPresent()){
             return;
         }
 
-        ImmutableAttack[] immutableAttacks = Attack.getAttacks(moves.toArray(new String[0]));
-        if (Arrays.stream(immutableAttacks).anyMatch(Objects::isNull)){
-            Logger.getGlobal().info("Could not get attacks from " + moves.stream().reduce("", (a, b) -> a + " " + b));
-            return;
+        Pokemon p = PokemonFactory.create(nameFormItem.Species.get());
+        if (nameFormItem.Form != null && !nameFormItem.Form.isEmpty()){
+            p.setForm(nameFormItem.Form);
         }
-        Attack[] attacks = Arrays.stream(immutableAttacks).map(ImmutableAttack::ofMutable).toArray(Attack[]::new);
+        p.setMoveset(movesAndIvs.Moveset.get());
+        p.setAbility(ability.Ability);
+        p.setNature(nature.NatureValue.get());
+        p.getStats().setEVs(evs.evStore);
+        p.getStats().setIVs(movesAndIvs.IvStore);
+        p.getStats().recalculateStats();
+    }
 
-        Moveset moveset = new Moveset(attacks, ability.get());
-        Pokemon p = PokemonFactory.create(species.get());
+    private static class SmogonLineOne {
+        Optional<Species> Species;
+        public String Form = "";
+        public String Item;
+        public SmogonLineOne(String line){
+            String[] line1Split = line.split("@");
+            String name = line1Split[0].trim();
+            if (line1Split.length == 2){
+                Item = line1Split[1].trim();
+            }
 
-        if (!form.isEmpty()){
-            if (species.get().hasForm(form)){
-                p.setForm(form);
+            String[] nameSplit = name.split("-");
+            if (nameSplit.length >= 2){
+
+                name = nameSplit[0].trim();
+                Form = nameSplit[1].trim()
+                        .replace("Alola", "alolan")
+                        .replace("Galar", "galarian");
+
+                if (nameSplit.length > 2){
+                    Form = nameSplit[1] + nameSplit[2];
+                }
+
+                if (Form.equals("F")){
+                    Form = "female";
+                }
+
+                if (Form.equals("o")){
+                    name = name + "-o";
+                    Form = "";
+                }
+
+                if (Form.equals("Z")){
+                    name = name + "-Z";
+                    Form = "";
+                }
+
+                if (Form.equals("Oh")){
+                    name = name + "-Oh";
+                    Form = "";
+                }
+
+                if (Form.equals("Dawn Wings")){
+                    Form = "dawn";
+                }
+
+                if (Form.equals("Dusk Mane")){
+                    Form = "dusk";
+                }
+
+                if (Form.equals("Small") || Form.equals("Super") || Form.equals("Large")){
+                    Form = "";
+                }
+            }
+
+            Species = PixelmonSpecies.fromName(name).getValue();
+            if (!Species.isPresent()){
+                Logger.getGlobal().info("Could not get species from name " + name);
             }
             else{
-                Logger.getGlobal().info("Could not set form " + form + " on " + p.getDisplayName());
+                if (!Form.isEmpty()){
+                    if (!Species.get().hasForm(Form)){
+                        Logger.getGlobal().info("Could not get form " + Form + " for species " + name);
+                        Species = Optional.empty();
+                    }
+                }
             }
         }
+    }
 
-        p.setNature(nature);
-        p.getIVs().copyIVs(ivStore);
-        p.getStats().recalculateStats();
+    private static class SmogonLineTwo{
+        Optional<Ability> Ability;
+
+        public SmogonLineTwo(String line){
+            String abilityName = line.replace("Ability:", "").trim().replace(" ", "");
+
+            Ability =  AbilityRegistry.getAbility(abilityName);
+            if (!Ability.isPresent()){
+                //some abilities have mismatched spellings
+                abilityName = abilityName
+                        .replace("ShellArmor", "ShellArmour")
+                        .replace("BattleArmor", "BattleArmour");
+
+                Ability =  AbilityRegistry.getAbility(abilityName);
+                if (!Ability.isPresent()){
+                    Logger.getGlobal().info("Could not get ability from name " + abilityName);
+                }
+            }
+        }
+    }
+
+    private static class SmogonLineThree{
+        EVStore evStore;
+        public SmogonLineThree(String line){
+            int hpEV = 0;
+            int atkEV = 0;
+            int defEv = 0;
+            int spAtkEv = 0;
+            int spDefEv = 0;
+            int speEv = 0;
+            String[] evs = line.replace("EVs:", "").split("/");
+            for (String ev : evs) {
+                String[] split = ev.trim().split(" ");
+                if (split.length != 2){
+                    break;
+                }
+                switch (split[1].trim()){
+                    case "HP": {
+                        hpEV = Integer.parseInt(split[0]);
+                        break;
+                    }
+                    case "Atk": {
+                        atkEV = Integer.parseInt(split[0]);
+                        break;
+                    }
+                    case "Def": {
+                        defEv = Integer.parseInt(split[0]);
+                        break;
+                    }
+                    case "SpA": {
+                        spAtkEv = Integer.parseInt(split[0]);
+                        break;
+                    }
+                    case "SpD": {
+                        spDefEv = Integer.parseInt(split[0]);
+                        break;
+                    }
+                    case "Spe": {
+                        speEv = Integer.parseInt(split[0]);
+                        break;
+                    }
+                }
+            }
+            evStore = new EVStore(hpEV, atkEV, defEv, spAtkEv, spDefEv, speEv);
+        }
+    }
+
+    private static class SmogonLineFour{
+        Optional<Nature> NatureValue;
+
+        public SmogonLineFour(String line){
+            String nature = line.replace("Nature", "").trim();
+
+            Nature tempNatureValue = Nature.natureFromString(nature);
+            if (tempNatureValue == null){
+                Logger.getGlobal().info("Could not get nature from name " + nature);
+                NatureValue = Optional.empty();
+            }
+            else{
+                NatureValue = Optional.of(tempNatureValue);
+            }
+        }
+    }
+
+    private static class SmogonFinalLines{
+        IVStore IvStore;
+        Optional<Moveset> Moveset;
+
+        public SmogonFinalLines(String[] lines){
+            String[] moves = new String[lines.length];
+            IvStore = new IVStore(31, 31, 31, 31, 31, 31);
+            for (int i = 0; i < lines.length; i++){
+                String moveString = lines[i].substring(2).trim();
+
+                String[] hiddenPowerSplit = moveString.split("Hidden Power");
+                if (hiddenPowerSplit.length == 2){
+                    moveString = "Hidden Power";
+                    Element ele = Element.parseType(hiddenPowerSplit[1].trim());
+                    IvStore = HiddenPower.getOptimalIVs(ele);
+                }
+
+                moves[i] = moveString;
+            }
+
+            ImmutableAttack[] immutableAttacks = Attack.getAttacks(moves);
+            if (Arrays.stream(immutableAttacks).anyMatch(Objects::isNull)){
+                Logger.getGlobal().info("Could not get attacks from " + Arrays.stream(moves).reduce("", (a, b) -> a + " " + b));
+                Moveset = Optional.empty();
+            }
+            else {
+                Attack[] attacks = Arrays.stream(immutableAttacks).map(ImmutableAttack::ofMutable).toArray(Attack[]::new);
+
+                Moveset = Optional.of(new Moveset(attacks, null));
+            }
+        }
     }
 }
