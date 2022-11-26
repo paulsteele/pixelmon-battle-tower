01615bc91148a7b846cc1f7a0487992f725ba763
Paul Steele <paul@paul-steele.com>
2022-11-25 21:18:53 -0500

    put items in creative tab

--- src/main/java/com/pixelmon/battletower/MovesetSelectiveResourceReloadListener.java
+++ src/main/java/com/pixelmon/battletower/MovesetSelectiveResourceReloadListener.java
@@ -1,9 +1,9 @@
 package com.pixelmon.battletower;
 
+import net.minecraft.client.resources.ReloadListener;
+import net.minecraft.profiler.IProfiler;
 import net.minecraft.resources.IResourceManager;
 import net.minecraft.util.ResourceLocation;
-import net.minecraftforge.resource.IResourceType;
-import net.minecraftforge.resource.ISelectiveResourceReloadListener;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -11,10 +11,9 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.List;
-import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
-public class MovesetSelectiveResourceReloadListener implements ISelectiveResourceReloadListener {
+public class MovesetSelectiveResourceReloadListener extends ReloadListener {
     static Logger logger = LogManager.getLogger(BattleTowerMain.ModId);
     private MovesetRepository repository;
 
@@ -23,12 +22,24 @@ public class MovesetSelectiveResourceReloadListener implements ISelectiveResourc
         this.repository = repository;
     }
 
+    private List<String> GetLinesOfResourceFile(IResourceManager manager, ResourceLocation location) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(manager.getResource(location).getInputStream()));
+        List<String> contents = reader.lines().collect(Collectors.toList());
+        reader.close();
+        return contents;
+    }
+
     @Override
-    public void onResourceManagerReload(IResourceManager resourceManager, Predicate<IResourceType> resourcePredicate) {
+    protected Object prepare(IResourceManager resourceManager, IProfiler profiler) {
+        return null;
+    }
+
+    @Override
+    protected void apply(Object obj, IResourceManager resourceManager, IProfiler profiler) {
         repository.ClearRawMovesets();
 
         try {
-             List<String> tiers = GetLinesOfResourceFile(resourceManager, new ResourceLocation("pixelmonbattletower.movesets", "tiers.txt"));
+            List<String> tiers = GetLinesOfResourceFile(resourceManager, new ResourceLocation("pixelmonbattletower.movesets", "tiers.txt"));
 
             for (String tier : tiers) {
                 for (ResourceLocation resourceLocation : resourceManager.listResources(tier, s -> s.endsWith(".txt"))) {
@@ -39,12 +50,5 @@ public class MovesetSelectiveResourceReloadListener implements ISelectiveResourc
         catch (Exception e) {
             logger.error(e.getMessage(), e);
         }
-    }
-
-    private List<String> GetLinesOfResourceFile(IResourceManager manager, ResourceLocation location) throws IOException {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(manager.getResource(location).getInputStream()));
-        List<String> contents = reader.lines().collect(Collectors.toList());
-        reader.close();
-        return contents;
     }
 }
